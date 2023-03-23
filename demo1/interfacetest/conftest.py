a7c1c7fca6c26dfb242b7f9db10ed541a49059cb
daiao <1298481990@qq.com>
2023-03-23 16:46:35 +0800

    旧文件清理

--- demo1/interfacetest/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="function", params=["张三", "李四"])
-def my_fixture(request):
-    print("前置处理器")
-    yield request.param
-    print("后置处理器")
--- demo1/interfacetest/usermanage/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="function")
-def user_fixture(request):
-    print("用户前置处理器")
-    yield
-    print("用户后置处理器")
